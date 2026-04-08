b04521dc2cc1cdba225859328efb65612081b429
ZSL-MSYS22 <zale.sebastian.latonio@student.ateneo.edu>
2026-04-08 18:17:07 +0800

    Added inline COA in models.py

---
+++ tapas/tapasapp/models.py
@@ -0,0 +1,27 @@
+# , ; , ; Zale Sebastian S. Latonio, 242494
+'''
+We hereby attest to the truth of the following facts:
+
+We have not discussed the Python code in our program with anyone
+other than my instructor or the teaching assistants assigned to this course.
+
+We have not used Python code obtained from another student, or
+any other unauthorized source, whether modified or unmodified.
+
+If any Python code or documentation used in our program was
+obtained from another source, it has been clearly noted with citations in the
+comments of our program.'''
+
+from django.db import models
+
+# Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(max_length=300)
+    prep_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    objects = models.Manager()
+
+    def __str__(self):
+        return str(self.pk) + ": " + self.name
+    
