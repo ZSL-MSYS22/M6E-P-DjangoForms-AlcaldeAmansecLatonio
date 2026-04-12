b9aaba8f08a28353026b426532664100b19df17e
louoodlesss <louoodles@gmail.com>
2026-04-13 07:42:28 +0800

    heavy edits on project

--- M6P-DjangoForms-AlcaldeAmansecLatonio/MyInventorySystem/MyInventoryApp/urls.py
+++ M6P-DjangoForms-AlcaldeAmansecLatonio/MyInventorySystem/MyInventoryApp/urls.py
@@ -1,3 +1,18 @@
+# Johann Miguel S. Alcalde, 240150 ; Samantha Louise F. Amansec, 230286 ; Zale Sebastian S. Latonio, 242494
+'''
+We hereby attest to the truth of the following facts:
+
+We have not discussed the Python code in our program with anyone
+other than my instructor or the teaching assistants assigned to this course.
+
+We have not used Python code obtained from another student, or
+any other unauthorized source, whether modified or unmodified.
+
+If any Python code or documentation used in our program was
+obtained from another source, it has been clearly noted with citations in the
+comments of our program.
+'''
+
 """
 URL configuration for MyInventorySystem project.
 
@@ -15,18 +30,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('view_suppliers/', views.view_suppliers, name='view_suppliers'),
-    path('view_water_bottles/', views.view_water_bottles, name='view_water_bottles'),
+    path('view_bottles/', views.view_bottles, name='view_bottles'),
     path('add_bottle/', views.add_bottle, name='add_bottle'),
     path('view_bottle_details/<int:pk>/', views.view_bottle_details, name='view_bottle_details'),
     path('delete_bottle/<int:pk>/', views.delete_bottle, name='delete_bottle'),
-    path('manage_account/', views.manage_account, name='manage_account'),
-    path("change_password/", views.change_password, name="change_password"),
-    path('delete_account/', views.delete_account, name='delete_account'),
+    path('manage_account/<int:pk>/', views.manage_account, name='manage_account'),
+    path('change_password/<int:pk>/', views.change_password, name='change_password'),
+    path('delete_account/<int:pk>/', views.delete_account, name='delete_account'),
     path('logout/', views.logout_view, name='logout'),
 ]
--- M6P-DjangoForms-AlcaldeAmansecLatonio/MyInventorySystem/MyInventoryApp/views.py
+++ M6P-DjangoForms-AlcaldeAmansecLatonio/MyInventorySystem/MyInventoryApp/views.py
@@ -1,3 +1,18 @@
+# Johann Miguel S. Alcalde, 240150 ; Samantha Louise F. Amansec, 230286 ; Zale Sebastian S. Latonio, 242494
+'''
+We hereby attest to the truth of the following facts:
+
+We have not discussed the Python code in our program with anyone
+other than my instructor or the teaching assistants assigned to this course.
+
+We have not used Python code obtained from another student, or
+any other unauthorized source, whether modified or unmodified.
+
+If any Python code or documentation used in our program was
+obtained from another source, it has been clearly noted with citations in the
+comments of our program.
+'''
+
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Supplier, WaterBottle, Account
 
@@ -5,11 +20,11 @@ from .models import Supplier, WaterBottle, Account
 def view_suppliers(request):
     suppliers = Supplier.objects.all()
     accounts = Account.objects.all()
-    return render(request, 'view_suppliers.html', {'suppliers': suppliers, 'accounts': accounts})
+    return render(request, 'MyInventoryApp/view_suppliers.html', {'suppliers': suppliers, 'accounts': accounts})
 
-def view_water_bottles(request):
+def view_bottles(request):
     water_bottles = WaterBottle.objects.all()
-    return render(request, 'view_water_bottles.html', {'water_bottles': water_bottles})
+    return render(request, 'MyInventoryApp/view_bottles.html', {'water_bottles': water_bottles})
 
 def add_bottle(request):
     if request.method == 'POST':
@@ -34,44 +49,42 @@ def add_bottle(request):
             supplied_by=supplied_by,
             current_quantity=current_quantity
         )
-        return redirect('view_water_bottles')
+        return redirect('view_bottles')
     else:
         suppliers = Supplier.objects.all()
-        return render(request, 'add_bottle.html', {'suppliers': suppliers})
+        return render(request, 'MyInventoryApp/add_bottle.html', {'suppliers': suppliers})
 
 def view_bottle_details(request, pk):
     bottle = get_object_or_404(WaterBottle, pk=pk)
-    return render(request, 'view_bottle_details.html', {'bottle': bottle})
+    return render(request, 'MyInventoryApp/view_bottle_details.html', {'bottle': bottle})
 
 
-def login_view(request): 
+def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         try:
             account = Account.objects.get(username=username, password=password)
+            # Store pk in session so we can use it across pages
             request.session['account_id'] = account.id
-            return redirect('view_water_bottles')
+            return redirect('view_suppliers')
         except Account.DoesNotExist:
-            error_message = 'Invalid username or password'
-            return render(request, 'login.html', {'error_message': error_message})
+            return render(request, 'MyInventoryApp/login.html', {'error_message': 'Invalid login'})
     else:
-        return render(request, 'login.html')
-    
+        return render(request, 'MyInventoryApp/login.html')
+
 def signup_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         if Account.objects.filter(username=username).exists():
-            error_message = 'Username already exists'
-            return render(request, 'signup.html', {'error_message': error_message})
+            return render(request, 'MyInventoryApp/signup.html', {'error_message': 'Account already exists'})
         else:
-            account = Account.objects.create(username=username, password=password)
-            request.session['account_id'] = account.id
-            return redirect('view_water_bottles')
+            Account.objects.create(username=username, password=password)
+            return render(request, 'MyInventoryApp/login.html', {'success_message': 'Account created successfully'})
     else:
-        return render(request, 'signup.html')
-    
+        return render(request, 'MyInventoryApp/signup.html')
+
 def logout_view(request):
     request.session.flush()
     return redirect('login')
@@ -79,38 +92,39 @@ def logout_view(request):
 def delete_bottle(request, pk):
     bottle = get_object_or_404(WaterBottle, pk=pk)
     bottle.delete()
-    return redirect('view_water_bottles')
+    return redirect('view_bottles')
 
-def manage_account(request):
-    account = get_object_or_404(Account, pk=request.session.get('account_id'))
-    return render(request, 'manage_account.html', {'account': account})
+def manage_account(request, pk):
+    account = get_object_or_404(Account, pk=pk)
+    return render(request, 'MyInventoryApp/manage_account.html', {'account': account})
 
-def delete_account(request):
-    account = get_object_or_404(Account, pk=request.session.get('account_id'))
+# pk passed via URL; used to filter and delete the specific account record
+def delete_account(request, pk):
+    account = get_object_or_404(Account, pk=pk)
     account.delete()
     request.session.flush()
-    return redirect('login') 
+    return redirect('login')
 
-def change_password(request):
-    account = get_object_or_404(Account, pk=request.session.get('account_id'))
+# pk passed via URL to identify which account's password to change
+def change_password(request, pk):
+    account = get_object_or_404(Account, pk=pk)
     if request.method == 'POST':
         currentPassword = request.POST.get('currentPassword')
         newPassword = request.POST.get('newPassword')
-        confirmPassword = request.POST.get('confirmPassword') 
+        confirmPassword = request.POST.get('confirmPassword')
         if currentPassword != account.password:
-            error_message = 'Current password is incorrect'
-            return render(request, 'change_password.html', {'account': account, 'error_message': error_message})
+            return render(request, 'MyInventoryApp/change_password.html', {
+                'account': account,
+                'error_message': 'Current password is incorrect'
+            })
         elif newPassword != confirmPassword:
-            error_message = 'New password and confirm password do not match'
-            return render(request, 'change_password.html', {'account': account, 'error_message': error_message})
+            return render(request, 'MyInventoryApp/change_password.html', {
+                'account': account,
+                'error_message': 'New password and confirm password do not match'
+            })
         else:
-            account.password = newPassword
-            account.save()
-            return redirect('manage_account')
-
-
-
-
-
-
-
+            # Update the specific account record using filter + update
+            Account.objects.filter(pk=pk).update(password=newPassword)
+            return redirect('manage_account', pk=pk)
+    else:
+        return render(request, 'MyInventoryApp/change_password.html', {'account': account})
